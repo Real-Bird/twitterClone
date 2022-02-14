f1c906645b3f776790eadb4c4bae6c5b3de25a88
RealBird <marbil815@gmail.com>
2022-02-14 23:32:27 +0900

    22-02-14

--- app/src/fBase.js
+++ app/src/fBase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -21,3 +22,4 @@ const app = initializeApp(firebaseConfig);
 
 export const authService = getAuth();
 export const dbService = getFirestore();
+export const storage = getStorage();
--- app/src/routes/Home.js
+++ app/src/routes/Home.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { dbService } from "fBase";
+import React, { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storage } from "fBase";
 import { collection, addDoc, query, getDocs, onSnapshot, orderBy } from "firebase/firestore";
 import Rweet from "components/Rweet";
+import { ref, uploadString } from "firebase/storage";
 
 const Home = ({ userObj }) => {
   const [rweet, setRweet] = useState("");
   const [rweets, setRweets] = useState([]);
-
+  const [attachement, setAttachment] = useState();
+  const fileClear = useRef();
   // const getRweets = async () => {
   //   const q = query(collection(dbService, "rweets"));
   //   const querySnapshot = await getDocs(q);
@@ -35,16 +38,19 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const docRef = await addDoc(collection(dbService, "rweets"), {
-        text: rweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-      });
-      setRweet("");
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+    const fileRef = ref(storage, `${userObj.uid}/${uuidv4()}`);
+    const response = await uploadString(fileRef, attachement, "data_url");
+    console.log(response);
+    // try {
+    //   const docRef = await addDoc(collection(dbService, "rweets"), {
+    //     text: rweet,
+    //     createdAt: Date.now(),
+    //     creatorId: userObj.uid,
+    //   });
+    //   setRweet("");
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
   }
   const onChange = (event) => {
     const {
@@ -52,11 +58,32 @@ const Home = ({ userObj }) => {
     } = event
     setRweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const { currentTarget: { result } } = finishedEvent;
+      setAttachment(result);
+    }
+    reader.readAsDataURL(theFile);
+  }
+  const onClearImgClick = () => {
+    setAttachment(null);
+    fileClear.current.value = null;
+  }
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} value={rweet} />
+        <input onChange={onFileChange} type="file" accept="image/*" ref={fileClear} />
         <input type="submit" value="Rweet" />
+        {attachement && <div>
+          <img src={attachement} width="50px" height="50px" />
+          <button onClick={onClearImgClick}>Clear</button>
+        </div>}
       </form>
       <span>
         {rweets.map((rweet) =>
