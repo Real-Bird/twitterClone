6a9818ed94d0fb91d4b3f136c2b21cf2640d0730
RealBird <marbil815@gmail.com>
2022-02-15 23:59:30 +0900

    22-02-15

--- app/src/routes/Profile.js
+++ app/src/routes/Profile.js
@@ -1,14 +1,29 @@
-import { authService } from "fBase";
-import React from "react";
+import { authService, dbService } from "fBase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
 
   }
+  const getMyRweets = async () => {
+    const q = query(
+      collection(dbService, "rweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  useEffect(() => {
+    getMyRweets();
+  }, [])
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
